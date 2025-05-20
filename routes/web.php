782d1b139087231231888dc67fcb47d265e96738
FebriTPP <febritripputra14@gmail.com>
2025-05-20 23:13:55 +0700

    Menambahkan autentikasi

--- routes/web.php
+++ routes/web.php
@@ -5,8 +5,12 @@ use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\RegisterController;
 
 Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
-Route::get('/register', [RegisterController::class, 'showRegistrationForm'])->name('register');
+Route::post('/login', [LoginController::class, 'login'])->name('login.post');
 
+Route::get('/register', [RegisterController::class, 'showRegistrationForm'])->name('register');
+Route::post('/register', [RegisterController::class, 'register'])->name('register.post');
+
+Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
 
 Route::get('/', function () {
     return view('welcome');
